b2a5933f38de445248e667eb78fea73d0599ab89
MOHAK CHATURVEDI <mohakchaturvedi888@gmail.com>
2026-07-14 15:16:40 +0530

    21 ms (5.89%) 19.4 MB (75.32%) 07-14-2026

--- LeetCode/Easy/0242-valid-anagram/0242-valid-anagram.py
+++ LeetCode/Easy/0242-valid-anagram/0242-valid-anagram.py
@@ -1,3 +1,24 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-       return sorted(s) == sorted(t)
+        freq = {}
+
+        for i in range(len(s)):
+            if s[i] in freq:
+                freq[s[i]] = freq[s[i]] + 1
+            else:
+                freq[s[i]] = 1
+
+        for j in range(len(t)):
+            if t[j] not in freq:
+                return False
+
+            freq[t[j]] = freq[t[j]] - 1
+
+            if freq[t[j]] < 0:
+                return False
+
+        for value in freq.values():
+            if value != 0:
+                return False
+
+        return True
