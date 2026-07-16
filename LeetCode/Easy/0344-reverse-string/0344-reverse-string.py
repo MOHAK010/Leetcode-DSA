fc583c9a42a51cfc22ae9147d7855961b28939ab
MOHAK CHATURVEDI <mohakchaturvedi888@gmail.com>
2026-07-16 21:15:22 +0530

    3 ms (45.99%) 23.6 MB (18.71%) 07-16-2026

--- LeetCode/Easy/0344-reverse-string/0344-reverse-string.py
+++ LeetCode/Easy/0344-reverse-string/0344-reverse-string.py
@@ -1,6 +1,13 @@
 class Solution:
     def reverseString(self, s: List[str]) -> None:
-        s.reverse()
+        left = 0
+        right = len(s) - 1
+        while left < right:
+            s[left], s[right] = s[right], s[left]   # Swap
+
+            left += 1
+            right -= 1
+        return 
         """
         Do not return anything, modify s in-place instead.
         """
