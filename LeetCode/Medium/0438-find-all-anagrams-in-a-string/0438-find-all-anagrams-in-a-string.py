7d987c452ee4480906e9c4795a60696f34da8a4c
MOHAK CHATURVEDI <mohakchaturvedi888@gmail.com>
2026-08-03 14:58:27 +0530

    40 ms (57.14%) 20 MB (15.95%) 08-03-2026

---
+++ LeetCode/Medium/0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
@@ -0,0 +1,42 @@
+class Solution:
+    def findAnagrams(self, s: str, p: str) -> List[int]:
+
+        if len(p) > len(s):
+            return []
+
+        freq_p = {}
+        freq_window = {}
+
+        # p ki frequency
+        for ch in p:
+            if ch not in freq_p:
+                freq_p[ch] = 1
+            else:
+                freq_p[ch] += 1
+
+        left = 0
+        ans = []
+
+        for right in range(len(s)):
+
+            # Right add
+            if s[right] not in freq_window:
+                freq_window[s[right]] = 1
+            else:
+                freq_window[s[right]] += 1
+
+            # Window ko fixed size me rakho
+            if (right - left + 1) > len(p):
+
+                freq_window[s[left]] -= 1
+
+                if freq_window[s[left]] == 0:
+                    del freq_window[s[left]]
+
+                left += 1
+
+            # Match mila
+            if freq_window == freq_p:
+                ans.append(left)     # ⭐ Starting index store
+
+        return ans
