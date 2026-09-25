4f447d08274a3fff2abece9662b80dedb238b0e4
MOHAK CHATURVEDI <mohakchaturvedi888@gmail.com>
2026-09-26 05:15:02 +0530

    0 ms (100%) 23.7 MB (5.44%) 09-26-2026

--- LeetCode/Easy/0344-reverse-string/0344-reverse-string.py
+++ LeetCode/Easy/0344-reverse-string/0344-reverse-string.py
@@ -1,14 +1,6 @@
 class Solution:
-    def reverseString(self, s: List[str]) -> None:
-        left = 0
-        right = len(s) - 1
-        while left < right:
-            s[left], s[right] = s[right], s[left]   # Swap
-
-            left += 1
-            right -= 1
-        return 
+    def reverseString(self, s: list[str]) -> None:
         """
         Do not return anything, modify s in-place instead.
         """
-       
+        s.reverse()
